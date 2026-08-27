3a9f1541e8cbcdccb7197f825f40eebcd1b82661
Nelson Ramírez <xhyskoax@gmail.com>
2026-08-26 21:55:44 -0400

    Se realizas asociaciones entre las entidades Usuario, Publicacion y Comentario

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,35 @@
 import Usuario from "./Usuario.model.js";
+import Comentario from "./Comentario.model.js";
+import Publicacion from "./publicacion.model.js";
 
 
 //RELACIONES DE ERD
 
+//UNO A MUCHOS ENTRE USUARIO Y PUBLICACIÓN
+Usuario.hasMany(Publicacion, {
+    foreignKey: "usuario_id"
+});
+Publicacion.belongsTo(Usuario, {
+    foreignKey: "usuario_id"
+});
 
-export default Usuario;
+//UNO A MUCHOS ENTRE USUARIO Y COMENTARIO
+Usuario.hasMany(Comentario, {
+    foreignKey: "usuario_id"
+});
+Comentario.belongsTo(Usuario, {
+    foreignKey: "usuario_id"
+});
+
+//UNO A MUCHOS ENTRE PUBLICACION Y COMENTARIO
+Publicacion.hasMany(Comentario, {
+    foreignKey: "publicacion_id"
+});
+Comentario.belongsTo(Publicacion, {
+    foreignKey: "publicacion_id"
+});
+
+
+export default {
+    Usuario, Comentario, Publicacion
+}
